40ab2ec409169e96c08e370f687169b2f694fd20
dmvdementev <dmvdementev@MS-D60705-66>
2016-07-15 19:10:48 +0300

    +initial (session, grid)

---
+++ BooksLibrary/Controllers/BooksController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BooksLibrary.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Book
+        public ActionResult List()
+        {
+            var books = new List<BookInfo>();
+            books.Add(new BookInfo()
+            {
+                Id = 1,
+                Title = "Книга",
+                Authors = new List<Author>()
+                {
+                   new Author()
+                   {
+                       Name = "Имя1",
+                       Surname = "Фамилия1"
+                   },
+                   new Author()
+                   {
+                       Name = "Имя2",
+                       Surname = "Фамилия2"
+                   },
+                   
+                   new Author()
+                   {
+                       Name = "Имя3",
+                       Surname = "Фамилия3"
+                   }
+                },
+                Pages = 300,
+                Year = "1907",
+                Publisher = "Tmb",
+                ISBN = "90906890423423",
+                Cover = new byte[0],
+            });
+
+            Session["books"] = books;
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetBooks()
+        {
+            return Json(Session["books"], JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public void AddBook(BookInfo book)
+        {
+            var books = Session["books"] as List<BookInfo>;
+            books.Add(book);
+        }
+    }
+}
---
+++ BooksLibrary/Models/BookInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BooksLibrary
+{
+    public class BookInfo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<Author> Authors { get; set; }
+        public long? Pages { get; set; }
+        public string Publisher { get; set; }
+        public string Year { get; set; }
+        public string ISBN { get; set; }
+        public byte[] Cover { get; set; }
+    }
+
+    public class Author
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
